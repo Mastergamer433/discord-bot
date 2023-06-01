974124579b740cad3c6da8bfbe5b7a843b1fe79a
Elis Odenhage <teknikgillaren@gmail.com>
2023-06-01 10:20:14 +0200

    latest

--- migrations/2023-05-30-184445_create-points/up.sql
+++ migrations/2023-05-30-184445_create-points/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE balance(
-  `id` INTEGER AUTO_INCREMENT,
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
   `user_id` VARCHAR(191) NOT NULL,
   `points` FLOAT NOT NULL,
   primary key(`id`)
